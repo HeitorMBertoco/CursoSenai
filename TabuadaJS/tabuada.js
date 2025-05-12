e6b33f056c545930a37492bc9e4b4a6f4b7fcf9a
Heitor <hmbertoco@gmail.com>
2025-05-12 09:01:41 -0300

    Versao com array

--- TabuadaJS/tabuada.js
+++ TabuadaJS/tabuada.js
@@ -1,6 +1,9 @@
 function calcularTabuada() {
-    var valor = document.getElementById('numero').value
-    var tabelaHTML = "";
+    let valor = document.getElementById('numero').value
+    let tabelaHTML = "";
+    let arr_numbers = [];
+    let res_arr = [];
+
     tabelaHTML += "<table>";
     tabelaHTML += "<tbody>";
 
@@ -10,13 +13,17 @@ function calcularTabuada() {
         
     }
     for (let i = 0; i <= 10; i++) {
+        arr_numbers.push(i);
+        res_arr.push(valor * i);
+    }
+    for (let i = 0; i <= 10; i++) {
         
             tabelaHTML +="<tr>";
-            tabelaHTML +="<td>"+ i +"</td>";
+            tabelaHTML +="<td>"+ arr_numbers[i] +"</td>";
             tabelaHTML +="<td>x</td>";
             tabelaHTML +="<td>"+ valor +"</td>";
             tabelaHTML +="<td>=</td>";
-            tabelaHTML +='<td>'+ (valor * i)+'</td>';
+            tabelaHTML +='<td>'+ (res_arr[i])+'</td>';
             tabelaHTML +='</tr>';
 
                 
