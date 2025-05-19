7faa132f9883e4215f97c7e15d71d745f809c994
Savioaviao <hmbertoco@gmail.com>
2025-05-19 09:29:11 -0300

    matrizes

--- Matrizes/Matriz.js
+++ Matrizes/Matriz.js
@@ -1,11 +1,17 @@
 let matriz = [
-    ['indice','0','1','2','3','4'],
-    ['0','beterraba','abacaxi','tesoura','lua','banana'],
-    ['1',' javali',' macaco',' cachorro',' gato',' cor'],
-    ['2','alicate','maionese','maçã','maionese','joaquim'],
-    ['3','ivonete','louça','balao','tangerina','jupiter'],
-    ['4','beterraba','tião','Carlos','','Beterraba']
+    ['beterraba','abacaxi','tesoura','lua','banana'],
+    [' javali',' macaco',' cachorro',' gato',' cor'],
+    ['alicate','maionese','maçã','maionese','joaquim'],
+    ['ivonete','louça','balao','tangerina','jupiter'],
+    ['beterraba','tião','Carlos','','Beterraba']
 ]
+matriz[1][1] = 'Goku'
+matriz[0][1] = 'Dinossauro'
+matriz[4][1] = ''
+matriz.splice(3,1)
+
+matriz.push(['','Carla','','Jessica',''])
+
 for(i = 0;i < matriz.length;i++){
     console.log(matriz[i])
 }
