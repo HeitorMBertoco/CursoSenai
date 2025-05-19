d0ee97c3610f4e77b649fe0dc3bba6d3bb3f2839
Savioaviao <hmbertoco@gmail.com>
2025-05-19 08:41:47 -0300

    Matrizes

---
+++ Matrizes/Matriz.js
@@ -0,0 +1,11 @@
+let matriz = [
+    ['indice','0','1','2','3','4'],
+    ['0','beterraba','abacaxi','tesoura','lua','banana'],
+    ['1',' javali',' macaco',' cachorro',' gato',' cor'],
+    ['2','alicate','maionese','maçã','maionese','joaquim'],
+    ['3','ivonete','louça','balao','tangerina','jupiter'],
+    ['4','beterraba','tião','Carlos','','Beterraba']
+]
+for(i = 0;i < matriz.length;i++){
+    console.log(matriz[i])
+}
