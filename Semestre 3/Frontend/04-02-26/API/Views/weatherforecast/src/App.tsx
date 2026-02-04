2754fbc0d600110b70a156fe968960fc293b9fbf
heitor <hmbertoco@gmail.com>
2026-02-04 13:26:00 -0300

    Adição aula 04-02-26

---
+++ Semestre 3/Frontend/04-02-26/API/Views/weatherforecast/src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { Weather } from "./types/weather";
+
+function App() {
+  const [weatherData, setWeatherData] = useState<Weather[]>([]);
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await fetch("http://localhost:5139/weatherforecast/");
+      const data: Weather[] = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Weather Forecast</h1>
+
+      {weatherData.map((item, index) => (
+        <div key={index}>
+          <h3>{item.date}</h3>
+          <p>
+            Temperature: {item.temperatureC} °C / {item.temperatureF} °F
+          </p>
+          <p>Summary: {item.summary}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
